eaf135bce06e3936207dd7c9722a9a65aad04c71
gyming1849 <1337374186@qq.com>
2021-05-31 01:24:28 +0800

    GYM is so handsome!

--- desktop/src/org/gyming/tank/desktop/DesktopLauncher.java
+++ desktop/src/org/gyming/tank/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package org.gyming.tank.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import org.gyming.game.GDX_Tank;
+import org.gyming.tank.client.ClientGUI;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GDX_Tank(), config);
+		new LwjglApplication(new ClientGUI(), config);
 	}
 }
