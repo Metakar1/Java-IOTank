2d6d9f4bd727454c0c5fbe17e72fd7e0f2857894
NicoDafaGood <30798992+NicoDafaGood@users.noreply.github.com>
2021-06-02 18:55:08 +0800

    114514

--- core/src/org/gyming/tank/client/Client.java
+++ core/src/org/gyming/tank/client/Client.java
@@ -13,43 +13,5 @@ import java.util.TreeSet;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Client {
-    public static int test1 = 0, test2 = 0;
-    private static String serverAddress = "192.168.123.74";
-    private static int port = 7650;
-    public int nowFrame, lastFireFrame;
-    String name;
-    String room;
-    MsgIO S2C, C2S;
-    LinkedBlockingQueue<GameFrame> download;
-    LinkedBlockingQueue<String> queue;
-    ActionGroup actionGroup;
 
-    public Client() {
-        download = new LinkedBlockingQueue<>();
-        queue = new LinkedBlockingQueue<>();
-    }
-
-    public void BuildConnection() {
-        nowFrame = 0;
-        lastFireFrame = 0;
-        try {
-            C2S = new MsgIO(new Socket(serverAddress, port));
-            S2C = new MsgIO(new Socket(serverAddress, port));
-            S2C.send(name);
-            C2S.send(name);
-            Gson gson = new Gson();
-            C2S.send(gson.toJson(new ConnectMsg("join", name, room)));
-            System.out.println("GYMing is so awful!!!!");
-
-            Thread downloader = new Thread(new ClientDownloader(this, S2C));
-            Thread painter = new Thread(new ClientPainter(this));
-            Thread listener = new Thread(new ClientListener(this, C2S, queue));
-            downloader.start();
-            painter.start();
-            listener.start();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
--- core/src/org/gyming/tank/client/ClientListener.java
+++ core/src/org/gyming/tank/client/ClientListener.java
@@ -6,12 +6,12 @@ import java.io.IOException;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ClientListener implements Runnable {
-    Client client;
+    TankGame game;
     MsgIO C2S;
     LinkedBlockingQueue<String> queue;
 
-    public ClientListener(Client client, MsgIO C2S, LinkedBlockingQueue<String> queue) {
-        this.client = client;
+    public ClientListener(TankGame game, MsgIO C2S, LinkedBlockingQueue<String> queue) {
+        this.game = game;
         this.C2S = C2S;
         this.queue = queue;
     }
@@ -22,7 +22,7 @@ public class ClientListener implements Runnable {
             if (!queue.isEmpty()) {
                 try {
                     C2S.send(queue.peek());
-                    Client.test1++;
+                    TankGame.test1++;
                 }
                 catch (IOException e) {
                     e.printStackTrace();
