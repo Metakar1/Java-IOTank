7c4c566c40082a5f7a1a04b698cdf234f21f3f02
NicoDafaGood <30798992+NicoDafaGood@users.noreply.github.com>
2021-06-02 23:23:10 +0800

    114514

---
+++ core/src/org/gyming/tank/test/RunClient.java
@@ -0,0 +1,10 @@
+package org.gyming.tank.test;
+
+import org.gyming.tank.client.TankGame;
+
+public class RunClient {
+    public static void main(String[] args) {
+        TankGame client = new TankGame();
+
+    }
+}
---
+++ core/src/org/gyming/tank/test/RunServer.java
@@ -0,0 +1,15 @@
+package org.gyming.tank.test;
+
+import org.gyming.tank.server.Server;
+
+public class RunServer {
+    public static void main(String[] args) {
+        try {
+            Server server=new Server(7650);
+            server.startListen();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
